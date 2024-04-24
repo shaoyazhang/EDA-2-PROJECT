14a8730af7f9dc1d6629c9ced76dbc25673662bb
Iker Vieira Olea <iker.vieira01@estudiant.upf.edu>
2024-04-24 11:56:36 +0200

    Structures 1

---
+++ LAB1_code.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct{
+char name;
+int hp ;
+int atk;
+int def;
+
+}Character;
+typedef struct{
+
+}Skill;
+typedef struct{
+
+}Scenario;
+typedef struct{
+
+}Enemy;
+typedef struct{
+
+};
+typedef struct{
+
+};
